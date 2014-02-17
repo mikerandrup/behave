c0156badf38fe4d369ac916f56dd57cc0422cb98
Mike Randrup <github@mikerandrup.com>
2014-02-17 13:16:14 -0600

    Experimental fix for DB string issue (1/?)

--- BehaveCore/DBConnection.cs
+++ BehaveCore/DBConnection.cs
@@ -5,7 +5,8 @@ namespace Behave.BehaveCore
 {
     public static class DBConnection
     {
-        private const string AZURE_DB_STRING_NAME = "behave_azure_db_string";
+        private const string AZURE_MANDATED_PREFIX = "SQLCONNSTR_";
+        private const string AZURE_DB_STRING_NAME = AZURE_MANDATED_PREFIX + "BEHAVE_DB_STRING";
         private static string AzureConnectionString
         {
             get {
