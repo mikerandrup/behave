593d1e9430b99cf88dc82161a76b3fcf62370d39
Mike Randrup <github@mikerandrup.com>
2014-03-24 08:15:56 -0500

    Fixed missing file (source control tracking issue).

---
+++ BehaveCore/DataClasses/Composed/SingleHabitWithOccurrences.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Behave.BehaveCore.DataClasses
+{
+    public class SingleHabitWithOccurrences
+    {
+        public SingleHabitWithOccurrences()
+        {
+            Occurrences = new List<Occurrence>();
+        }
+        public Habit Habit { get; set; }
+        public List<Occurrence> Occurrences { get; set; }
+
+        public bool HasOccurrences
+        {
+            get
+            {
+                return Occurrences.Count > 0;
+            }
+        }
+
+        public string OccurrenceIdListAsCommaDelimitedString
+        {
+            get
+            {
+                if (Occurrences.Count > 0)
+                {
+                    string[] occIdStrings = new string[Occurrences.Count];
+                    int i = 0;
+                    foreach (var occ in Occurrences)
+                    {
+                        occIdStrings[i] = occ.OccurrenceId.ToString();
+                        i++;
+                    }
+                    return String.Join(",", occIdStrings);
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
