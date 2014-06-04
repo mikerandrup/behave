d62da54783317787973f6451e2d80382a262e25e
Rahlus Karl Morsi <github@metroplexweb.com>
2014-06-03 20:59:15 -0500

    Display List of Habits

--- BehaveWeb/content/javascript/uimain.js
+++ BehaveWeb/content/javascript/uimain.js
@@ -3,11 +3,12 @@
     'use strict';
 
     var elements = {
-        myElement: "#habitsList",
+        myElement: '#habitsList',
     },
 
     viewModel = null,
     myElementData = null,
+    allHabitsObject = null,
 
     methods = {
         attachElements: function (elementsObject) {
@@ -18,7 +19,14 @@
         parseHabitsData: function () {
             myElementData = elements.myElement.dataset.viewModel;
             viewModel = $.parseJSON(myElementData);
-            console.log(viewModel);
+            allHabitsObject = viewModel.HabitsWithOccurrences;
+
+            for (var i=0; i < allHabitsObject.length; i++){
+                methods.addHabitToHtml(allHabitsObject[i])
+            }
+        },
+        addHabitToHtml: function (toHTML) {
+            $('#habitsList').append('<li>' + toHTML.Habit.Title + '</li>');
         },
     },
 
